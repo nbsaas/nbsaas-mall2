59dcbb532858854f80dc9fcfe64fab2ce0d35dbe
ada.young <cng1985@gmail.com>
2017-06-04 10:54:41 +0800

    移除idea信息

--- data/src/main/java/com/quhaodian/data/apps/MakeApps.java
+++ data/src/main/java/com/quhaodian/data/apps/MakeApps.java
@@ -1,6 +1,7 @@
 package com.quhaodian.data.apps;
 
 import com.ada.article.entity.Article;
+import com.ada.article.entity.ArticleCatalog;
 import com.ada.article.entity.SensitiveCategory;
 import com.ada.data.entity.CatalogEntity;
 import com.ada.imake.ChainMake;
@@ -13,17 +14,17 @@ public class MakeApps {
 
 	public static void main(String[] args) {
 
-		File file = new File("D:\\mvnspace\\iwan\\iwan_web\\src\\main\\webapp\\WEB-INF\\ftl\\admin");
+		File file = new File("D:\\mvnspace\\iyelp\\web\\src\\main\\webapp\\WEB-INF\\ftl\\admin");
 		ChainMake make = new ChainMake(TemplateSimpleDir.class, TemplateHibernateDir.class);
-		make.setAction("com.ada.iwan.controller.admin");
+		make.setAction("com.quhaodian.actions.admin");
 		make.setView(file);
 		make.setDao(false);
 		make.setService(false);
 		make.setAction(true);
 		make.setView(true);
-		make.setMenus("1,49,50");
+		make.setMenus("1,57,58");
 		// UserOauthToken.
-		 make.makes(Article.class);
+		 make.makes(ArticleCatalog.class);
 		// make.makes(UserOschina.class,UserOauthWeibo.class,UserQQ.class,UserGitHub.class);
 		// new ChainMake().makes("com.ada.imakedemo.data.entity");
 
