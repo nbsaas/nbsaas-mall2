07455fa9c222ced78e7330fbd89bd72f041c9e5a
ada.young <735529126@qq.com>
2023-12-26 23:25:35 +0800

    修改代码

--- gates/front/src/main/java/com/nbsaas/boot/controller/shop/ShopCategoryController.java
+++ gates/front/src/main/java/com/nbsaas/boot/controller/shop/ShopCategoryController.java
@@ -16,6 +16,7 @@ import com.nbsaas.boot.shop.api.domain.simple.ShopCategorySimple;
 import com.nbsaas.boot.shop.api.apis.ShopCategoryApi;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,6 @@ import javax.annotation.Resource;
 /**
 *  对外控制器
 */
-@RequiresAuthentication
 @RestController
 @RequestMapping("/shopCategory")
 public class ShopCategoryController {
@@ -33,46 +33,21 @@ public class ShopCategoryController {
     @Resource
     private ShopCategoryApi shopCategoryApi;
 
-
-    @RequiresPermissions("shopCategory")
+    @Cacheable("shop")
     @RequestMapping("/search")
     public PageResponse <ShopCategorySimple> search(ShopCategorySearch request) {
         return shopCategoryApi.search(request);
     }
 
-    @RequiresPermissions("shopCategory")
+    @Cacheable("shop")
     @RequestMapping("/list")
     public ListResponse<ShopCategorySimple> list(ShopCategorySearch request) {
         return shopCategoryApi.list(request);
     }
 
-    /**
-    * 添加数据
-    *
-    * @param request
-    * @return
-    */
-    @RequiresPermissions("shopCategory")
-    @CreateData
-    @RequestMapping("/create")
-    public ResponseObject <ShopCategoryResponse> create(@Validated(AddOperator.class) ShopCategoryRequest request) {
-        return shopCategoryApi.create(request);
-    }
 
-   @RequiresPermissions("shopCategory")
-   @UpdateData
-   @RequestMapping("/update")
-   public ResponseObject<ShopCategoryResponse> update(@Validated(UpdateOperator.class) ShopCategoryRequest request) {
-       return shopCategoryApi.update(request);
-   }
 
-    @RequiresPermissions("shopCategory")
-    @RequestMapping("/delete")
-    public ResponseObject<?> delete(@Validated(DeleteOperator.class) ShopCategoryRequest request) {
-        return shopCategoryApi.delete(request);
-    }
 
-    @RequiresPermissions("shopCategory")
     @RequestMapping("/view")
     public ResponseObject <ShopCategoryResponse> view(@Validated(ViewOperator.class) ShopCategoryRequest  request) {
         return shopCategoryApi.view(request);
