ee14d559c0beb778b002d5bbb8cd7031545441ab
ada.young <735529126@qq.com>
2023-12-26 23:36:35 +0800

    修改代码

--- gates/admin/src/main/java/com/nbsaas/boot/controller/shop/ShopCategoryController.java
+++ gates/admin/src/main/java/com/nbsaas/boot/controller/shop/ShopCategoryController.java
@@ -16,6 +16,8 @@ import com.nbsaas.boot.shop.api.domain.simple.ShopCategorySimple;
 import com.nbsaas.boot.shop.api.apis.ShopCategoryApi;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,13 +35,14 @@ public class ShopCategoryController {
     @Resource
     private ShopCategoryApi shopCategoryApi;
 
-
+    @Cacheable("shopCategory")
     @RequiresPermissions("shopCategory")
     @RequestMapping("/search")
     public PageResponse <ShopCategorySimple> search(ShopCategorySearch request) {
         return shopCategoryApi.search(request);
     }
 
+    @Cacheable("shopCategory")
     @RequiresPermissions("shopCategory")
     @RequestMapping("/list")
     public ListResponse<ShopCategorySimple> list(ShopCategorySearch request) {
@@ -52,6 +55,7 @@ public class ShopCategoryController {
     * @param request
     * @return
     */
+    @CacheEvict("shopCategory")
     @RequiresPermissions("shopCategory")
     @CreateData
     @RequestMapping("/create")
@@ -59,13 +63,15 @@ public class ShopCategoryController {
         return shopCategoryApi.create(request);
     }
 
-   @RequiresPermissions("shopCategory")
+    @CacheEvict("shopCategory")
+    @RequiresPermissions("shopCategory")
    @UpdateData
    @RequestMapping("/update")
    public ResponseObject<ShopCategoryResponse> update(@Validated(UpdateOperator.class) ShopCategoryRequest request) {
        return shopCategoryApi.update(request);
    }
 
+    @CacheEvict("shopCategory")
     @RequiresPermissions("shopCategory")
     @RequestMapping("/delete")
     public ResponseObject<?> delete(@Validated(DeleteOperator.class) ShopCategoryRequest request) {
